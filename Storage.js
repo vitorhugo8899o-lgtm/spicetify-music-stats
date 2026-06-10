e635fe99f90f250b5dde0997ced3d8d75ed3d053
Vítor <vitorhugo8899o@gmail.com>
2026-06-10 16:26:07 -0300

    feat: Add getEventToday for today's events

--- Storage.js
+++ Storage.js
@@ -1,25 +1,25 @@
 window.MusicStats = window.MusicStats || {};
 
+const STORAGE_KEY = "music-stats:data";
+
+const todayDate = new Date().toISOString().split("T")[0];
+
 window.MusicStats.Storage = {
     saveEvent(event) {
-        const STORAGE_KEY = "music-stats:data";
-
         if (!event) {
             return "Erro: No event received!";
         }
-
-        const date = new Date().toISOString().split("T")[0];
 
         try {
             const raw = Spicetify.LocalStorage.get(STORAGE_KEY);
 
             const data = raw ? JSON.parse(raw) : {};
 
-            if (!data[date]) {
-                data[date] = [];
+            if (!data[todayDate]) {
+                data[todayDate] = [];
             }
 
-            data[date].push(event);
+            data[todayDate].push(event);
 
             Spicetify.LocalStorage.set(
                 STORAGE_KEY,
@@ -29,6 +29,20 @@ window.MusicStats.Storage = {
             return "Event save!";
         } catch (error) {
             return `Erro: ${error}`;
+        }
+    },
+    getEventToday() {
+        try {
+            const listToday = Spicetify.LocalStorage.get(STORAGE_KEY);
+
+            if (!listToday) {
+                return {};
+            }
+
+            return JSON.parse(listToday);
+        } catch (error) {
+            console.error(error);
+            return {};
         }
     }
 }
