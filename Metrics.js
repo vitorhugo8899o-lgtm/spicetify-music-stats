451323425e5c18339c10b2a686d829fde3d780bb
Vítor <vitorhugo8899o@gmail.com>
2026-06-11 15:55:53 -0300

    feat: Add a ranking of the most listened-to songs of the day.

--- Metrics.js
+++ Metrics.js
@@ -11,5 +11,25 @@ window.MusicStats.Metrics = {
 
         console.log(musicList[todayDate].length);
         return musicList[todayDate];
+    },
+    async getMostPlayedToday() {
+        const tracks = await window.MusicStats.Storage.getEventToday()
+        const todayDate = window.MusicStats.Utils.getLocalDate();
+
+        if (!tracks || !tracks[todayDate]) {
+            return "No events received.";
+        }
+
+        const counts = {};
+
+        for (let i = 0; i <= tracks[todayDate].length - 1; i++) {
+            const uri = tracks[todayDate][i].trackUri;
+            if (!counts[uri]) {
+                counts[uri] = { count: 0, trackName: tracks[todayDate][i].trackName, artists: tracks[todayDate][i].artists };
+            }
+            counts[uri].count++;
+        }
+        console.log(Object.values(counts).sort((a, b) => b.count - a.count))
+        return Object.values(counts).sort((a, b) => b.count - a.count);
     }
 };
