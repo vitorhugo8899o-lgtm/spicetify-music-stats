b1424b23f954e87228d870c61b6920aba97e6224
Vítor <vitorhugo8899o@gmail.com>
2026-06-12 12:09:30 -0300

    feat: add metric for which artist was the most listened to of the day.

--- Metrics.js
+++ Metrics.js
@@ -31,5 +31,31 @@ window.MusicStats.Metrics = {
         }
         console.log(Object.values(counts).sort((a, b) => b.count - a.count))
         return Object.values(counts).sort((a, b) => b.count - a.count);
+    },
+    async getTopArtistsToday() {
+        const tracks = await window.MusicStats.Storage.getEventToday()
+        const todayDate = window.MusicStats.Utils.getLocalDate();
+
+        if (!tracks || !tracks[todayDate]) {
+            return "No events received.";
+        }
+
+        const counts = {}
+
+        for (let i = 0; i < tracks[todayDate].length; i++) {
+            const artists = tracks[todayDate][i].artists;
+
+            for (let j = 0; j < artists.length; j++) {
+                const artist = artists[j];
+
+                if (!counts[artist.uri]) {
+                    counts[artist.uri] = { count: 0, name: artist.name, uri: artist.uri };
+                }
+                counts[artist.uri].count++;
+            }
+        }
+
+        console.log(Object.values(counts).sort((a, b) => b.count - a.count))
+        return Object.values(counts).sort((a, b) => b.count - a.count);
     }
-};
+}
