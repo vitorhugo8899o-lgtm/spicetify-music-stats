f7f33c3a648ead246e5051ac46a0ea698ad2b126
Vítor <vitorhugo8899o@gmail.com>
2026-06-25 19:34:32 -0300

    feat: final index template

--- index.js
+++ index.js
@@ -1,42 +1,105 @@
 function render() {
-    const musicTodayButton = Spicetify.React.createElement(
-        "button",
-        {
-            onClick: () => { window.MusicStats.Metrics.musicTodayCont() }
-        },
-        "Today Music"
-    );
+    const e = Spicetify.React.createElement
 
-    const mostPlayMusicToday = Spicetify.React.createElement(
-        "button",
-        {
-            onClick: () => { window.MusicStats.Metrics.getMostPlayedToday() }
-        },
-        "Most Play Music"
-    );
+    const [activePage, setActivePage] = Spicetify.React.useState("");
+    const [activeOption, setActiveOption] = Spicetify.React.useState("");
+    const [metricsData, setMetricsData] = Spicetify.React.useState(null);
+    const [isLoading, setIsLoading] = Spicetify.React.useState(false);
 
-    const totalTracks = Spicetify.React.createElement(
-        "div",
-        null,
-        "Rendering test"
-    );
+    Spicetify.React.useEffect(() => {
+        if (!activeOption) return;
 
-    const favoriteArtist = Spicetify.React.createElement(
-        "div",
-        null,
-        "Favorite artist"
-    );
+        async function loadMetrics() {
+            setIsLoading(true);
+            try {
+                const data = await buildMetricsToday();
 
-    return Spicetify.React.createElement(
+                setMetricsData(data);
+
+                if (activePage === "") {
+                    setActivePage("artists");
+                }
+            } catch (error) {
+                console.error("Error retrieving Spicetify metrics:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadMetrics();
+
+    }, [activeOption]);
+
+    let content;
+
+    if (!activeOption) {
+        content = e(window.MusicStats.Default.DefaultPage, null);
+    }
+
+    else if (isLoading) {
+        content = e("h1", null, "Loading metrics for the period...");
+    }
+
+    else if (typeof metricsData === "string") {
+        content = e("h1", null, metricsData);
+    }
+
+    else if (metricsData) {
+        switch (activePage) {
+            case "artists":
+                content = window.MusicStats.Artist.ArtistsPage
+                    ? e(window.MusicStats.Artist.ArtistsPage, metricsData.artist_metrics)
+                    : e("h1", null, "Artist Page");
+                break;
+
+            case "albums":
+                content = window.MusicStats.Album.AlbumPage
+                    ? e(window.MusicStats.Album.AlbumPage, metricsData.album_metrics)
+                    : e("h1", null, "Album Page");
+                break;
+
+            case "songs":
+                console.log("[index] music_metrics:", metricsData.music_metrics);
+                content = window.MusicStats.Song.SongsPage
+                    ? e(window.MusicStats.Song.SongsPage, metricsData.music_metrics)
+                    : e("h1", null, "Songs Page");
+                break;
+
+            case "overview":
+                content = window.MusicStats.Overview.OverviewPage
+                    ? e(
+                        window.MusicStats.Overview.OverviewPage,
+                        { overview_metrics: metricsData }
+                    )
+                    : e("h1", null, "OverView Page");
+                break;
+        }
+    }
+
+    return e(
         "div",
         {
             style: {
                 padding: "30px"
             }
         },
-        totalTracks,
-        favoriteArtist,
-        musicTodayButton,
-        mostPlayMusicToday
+
+        e(
+            "div",
+            {
+                style: {
+                    marginBottom: "40px",
+                    display: "flex",
+                    alignItems: "center"
+                }
+            },
+            window.MusicStats.Components.TabButton("Artists", "artists", activePage, setActivePage),
+            window.MusicStats.Components.TabButton("Albums", "albums", activePage, setActivePage),
+            window.MusicStats.Components.TabButton("Songs", "songs", activePage, setActivePage),
+            window.MusicStats.Components.TabButton("OverView", "overview", activePage, setActivePage),
+            window.MusicStats.Components.OptionsMenu(activeOption, setActiveOption)
+        ),
+
+        content
     );
 }
